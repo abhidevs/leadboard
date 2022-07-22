bd6647f9e1d6474707d72ede4cff71938d4da353
Abhik Das <abhikdas98765@gmail.com>
2022-07-22 08:37:45 +0530

    Added organisation model

--- leads/admin.py
+++ leads/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import User, Lead, Agent
+from .models import Organisation, User, Lead, Agent
 
 
 admin.site.register(User)
+admin.site.register(Organisation)
 admin.site.register(Lead)
 admin.site.register(Agent)
--- leads/models.py
+++ leads/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
     pass
+
+
+class Organisation(models.Model):
+    org_name = models.CharField(max_length=50)
+    admin = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.org_name
 
 
 class Lead(models.Model):
@@ -18,6 +27,17 @@ class Lead(models.Model):
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    organisation = models.ForeignKey(Organisation, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.email
+
+
+def post_user_created_signal(sender, instance, created, **kwargs):
+    if created:
+        org_name = f"{instance.first_name} {instance.last_name}"
+        print(org_name)
+        Organisation.objects.create(org_name=org_name, admin=instance)
+
+
+post_save.connect(post_user_created_signal, sender=User)
