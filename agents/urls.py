b7c0e5124b62b378712474de93e392456772322e
Abhik Das <abhikdas98765@gmail.com>
2022-07-24 19:10:38 +0530

    added crud functionalities for agents

--- agents/urls.py
+++ agents/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from agents.views import AgentCreateView, AgentListView
+from agents.views import AgentCreateView, AgentDeleteView, AgentDetailView, AgentListView, AgentUpdateView
 
 app_name = 'agents'
 
 urlpatterns = [
     path('', AgentListView.as_view(), name='agent-list'),
     path('create/', AgentCreateView.as_view(), name='agent-create'),
+    path('<int:pk>/', AgentDetailView.as_view(), name='agent-detail'),
+    path('<int:pk>/update/', AgentUpdateView.as_view(), name='agent-update'),
+    path('<int:pk>/delete/', AgentDeleteView.as_view(), name='agent-delete'),
 ]
--- agents/views.py
+++ agents/views.py
@@ -6,12 +6,13 @@ from leads.models import Agent
 
 
 class AgentListView(LoginRequiredMixin, generic.ListView):
-    template_name = "agents/agents_list.html"
+    template_name = "agents/agent_list.html"
     context_object_name = "agents"
-    
+
     def get_queryset(self):
-        return Agent.objects.all()
-    
+        user_org = self.request.user.organisation
+        return Agent.objects.filter(organisation=user_org)
+
 
 class AgentCreateView(LoginRequiredMixin, generic.CreateView):
     template_name = "agents/agent_create.html"
@@ -25,3 +26,36 @@ class AgentCreateView(LoginRequiredMixin, generic.CreateView):
         agent.organisation = self.request.user.organisation
         agent.save()
         return super(AgentCreateView, self).form_valid(form)
+
+
+class AgentDetailView(LoginRequiredMixin, generic.DetailView):
+    template_name = "agents/agent_detail.html"
+    context_object_name = "agent"
+
+    def get_queryset(self):
+        user_org = self.request.user.organisation
+        return Agent.objects.filter(organisation=user_org)
+
+
+class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
+    template_name = "agents/agent_update.html"
+    form_class = AgentModelForm
+
+    def get_queryset(self):
+        user_org = self.request.user.organisation
+        return Agent.objects.filter(organisation=user_org)
+
+    def get_success_url(self):
+        return reverse("agents:agent-list")
+
+
+class AgentDeleteView(LoginRequiredMixin, generic.DeleteView):
+    template_name = "agents/agent_delete.html"
+    context_object_name = "agent"
+
+    def get_queryset(self):
+        user_org = self.request.user.organisation
+        return Agent.objects.filter(organisation=user_org)
+
+    def get_success_url(self):
+        return reverse("agents:agent-list")
